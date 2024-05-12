8ed330882c9f8f11a898296f2e25703a0672dfd5
longhoang <long542.nt@gmail.com>
2024-05-12 14:54:48 +0700

    12/5/2024

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -72,6 +72,30 @@ const DetailQuiz = () => {
         }
 
     }
+    const handleFinishQuiz=()=>{
+        let payload={
+            quizId:+quizId,
+            answers:[]
+        }
+        let answers=[];
+        if(dataQuiz && dataQuiz.length>0){
+            dataQuiz.forEach(question=>{
+                let questionId=question.questionId;
+                let userAnswerId=[];
+                question.answers.forEach(a=>{
+                    if(a.isSelected===true){
+                        userAnswerId.push(a.id);
+                    }
+                })
+                answers.push({
+                    questionId:+questionId,
+                    userAnswerId:userAnswerId
+                })
+            })
+            payload.answers=answers;
+            console.log("final payload:",payload);
+        }
+    }
     return (
         <div className="detail-quiz-container">
             <div className='left-content'>
@@ -101,6 +125,10 @@ const DetailQuiz = () => {
                     className=' btn btn-secondary'
                     onClick={()=>{handleNext()}}
                     >Next</button>
+                     <button 
+                    className=' btn btn-warning'
+                    onClick={()=>{handleFinishQuiz()}}
+                    >Finish</button>
                 </div>
             </div>
             <div className='right-content'>
